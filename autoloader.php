39c6062e77762521537a336775618e6c02f04bd0
Theunis Johannes Botha <tjb@plista.com>
2014-05-28 15:30:22 +0200

    DPC-203 - definitive list

--- autoloader.php
+++ autoloader.php
@@ -1,22 +1,49 @@
 <?php
 
-namespace Plista {
-	class Autoloader {
-		public static function load($className) {
-			$className = ltrim($className, '\\');
-			$fileName  = '';
-			$namespace = '';
-			if ($lastNsPos = strrpos($className, '\\')) {
-				$namespace = substr($className, 0, $lastNsPos);
-				$className = substr($className, $lastNsPos + 1);
-				$fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-			}
-			$fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-			require $fileName;
+class PlistaAPIAutoloader {
+
+	/**
+	 * We keep the classes in memory to prevent us from doing unecessary file checks
+	 */
+	public static $classes = array(
+		'Plista\API\Interfaces\Response',
+		'Plista\API\Interfaces\ServiceDescription',
+		'Plista\API\Interfaces\API',
+		'Plista\API\Interfaces\Request',
+		'Plista\API\Interfaces\Service',
+		'Plista\API\Exception',
+		'Plista\API\Request\Http',
+		'Plista\API\Response\StdArray',
+		'Plista\API\Response\DataTables',
+		'Plista\API\Response\XML',
+		'Plista\API\Response\JSON',
+		'Plista\API\Response\MySQL',
+		'Plista\API\Response\StdObject'
+	);
+
+
+	public static function load($className) {
+
+		$className = ltrim($className, '\\');
+
+		if (!in_array($className, PlistaAPIAutoloader::$classes, true)){
+			return;
 		}
+
+		$fileName = 'plista-api' . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR;
+
+		if ($lastNsPos = strrpos($className, '\\')) {
+			$namespace = substr($className, 0, $lastNsPos);
+			$className = substr($className, $lastNsPos + 1);
+			$fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+		}
+
+		$fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+		require($fileName);
 	}
 }
 
-spl_autoload_register(array('Plista\Autoloader', 'load'));
+spl_autoload_register(array('PlistaAPIAutoloader', 'load'));
 
 ?>
